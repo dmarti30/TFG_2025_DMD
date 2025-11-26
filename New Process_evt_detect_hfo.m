1e75d6f1f24506969ec192ee6719042b61543621
dmarti30 <dmarti30@ucm.es>
2025-11-26 12:28:31 +0100

    Definitive New Process_evt_detect_hfo.m

--- New Process_evt_detect_hfo.m
+++ New Process_evt_detect_hfo.m
@@ -57,48 +57,60 @@ end
 
 
 %% ===== RUN =====
-function OutputFiles = Run(sProcess, sInputs) %#ok<DEFNU>   
-
-
+function OutputFiles = Run(sProcess, sInputs)    
 
 % Initialize the output files
 OutputFiles = {};
 
 for iFile = 1:length(sInputs)
-% Load the raw file descriptor
-isRaw = strcmpi(sInputs(iFile).FileType, 'raw');
+    % Load the raw file descriptor
+    isRaw = strcmpi(sInputs(iFile).FileType, 'raw');
 
-if isRaw
-    DataMat = in_bst_data(sInputs(iFile).FileName, 'F', 'events');
-    sFile = DataMat.F;
-else
-    DataMat = in_bst_data(sInputs(iFile).FileName, 'Time');
-    sFile = in_fopen(sInputs(iFile).FileName, 'BST-DATA');
+    if isRaw
+        DataMat = in_bst_data(sInputs(iFile).FileName, 'F', 'events');
+        sFile = DataMat.F;
+    else
+        DataMat = in_bst_data(sInputs(iFile).FileName, 'Time');
+        sFile = in_fopen(sInputs(iFile).FileName, 'BST-DATA');
+    end
+
+    % Create a new event that occurs at 1 second for all channels
+    % Basic events structure
+    sEvent = repmat(db_template('event'), 0);
+    iEvt =1;
+
+    % Create the event structure
+    sEvent(iEvt).label = sProcess.options.eventname.Value; % Get the event name from options
+    sEvent(iEvt).color = [1 0 0];
+    sEvent(iEvt).epochs = []; % Initialize epochs field
+    sEvent(iEvt).times = [1.570156250000000e+02]; % Set the event time
+    sEvent(iEvt).reactTimes = []; % Initialize reactTimes field
+    sEvent(iEvt).select = []; % Initialize select field
+    sEvent(iEvt).channels = []; % Specify channels if needed, empty for all channels
+    sEvent(iEvt).notes = []; % Initialize notes field
+
+    % Append the new event to the events field
+    sFile.events(end+1) = sEvent;
+
+     % Only save changes if something was detected
+        if ~isempty(sEvent)
+            % Report changes in .mat structure
+            if isRaw
+                DataMat.F = sFile;
+            else
+                DataMat.Events = sFile.events;
+            end
+            % Save file definition
+            bst_save(file_fullpath(sInputs(iFile).FileName), DataMat, 'v6', 1);
+            % Report number of detected events
+            nEvents =1;
+            bst_report('Info', sProcess, sInputs(iFile), sprintf('%d events detected in HFO', nEvents));
+        else
+            bst_report('Warning', sProcess, sInputs(iFile), ['No event detected. Please check the signal quality.']);
+        end
+        
+        OutputFiles = {sInputs.FileName};
+
 end
 end
 
-% Create a new event that occurs at 1 second for all channels
-
-% Basic events structure
-if ~isfield(sFile, 'events') || isempty(sFile.events)
-       sFile1.events = repmat(db_template('event'), 0);
-end
-iEvt=1;
-sEvent = sFile.events(iEvt);
-
-% Create the event structure
-sEvent.label = sProcess.options.eventname.Value; % Get the event name from options; % Set the event label
-sEvent.color = [1 0 0];
-% sEvent.epochs = []; % Initialize epochs field
-sEvent.times = [1.570156250000000e+02,5.059414062500000e+02,8.775820312500000e+02,1.125125000000000e+03,1.555351562500000e+03]; % Set the event time
-% sEvent.reactTimes = []; % Initialize reactTimes field
-% sEvent.select = []; % Initialize select field
-% sEvent.channels = []; % Specify channels if needed, empty for all channels
-sEvent.notes = []; % Initialize notes field
-
-
-% Append the new event to the events field
-sFile.events(end+1) = sEvent;
-OutputFiles{end+1} = sFile; % Append the updated file to the output
-
-end
